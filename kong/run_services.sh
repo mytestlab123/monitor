ea0630547f6f077be0b8c7c032938a0718d3a98d
Amit Karpe <amitkarpe@gmail.com>
2023-02-17 04:03:34 +0000

    Updated: run_services.sh

--- kong/run_services.sh
+++ kong/run_services.sh
@@ -2,12 +2,14 @@
 
 
 echo "Run prometheus"
-POD_NAME=$(kubectl get pods --namespace monitoring -l "app=prometheus,component=server" -o jsonpath="{.items[0].metadata.name}")
+POD_NAME=$(kubectl get pods --namespace monitoring -l "app.kubernetes.io/name=prometheus" -o jsonpath="{.items[0].metadata.name}")
+# POD_NAME=$(kubectl get pods --namespace monitoring -l "app=prometheus,component=server" -o jsonpath="{.items[0].metadata.name}")
 
 kubectl --namespace monitoring port-forward $POD_NAME 9090 &
 
 echo "Run grafana"
-POD_NAME=$(kubectl get pods --namespace monitoring -l "app.kubernetes.io/instance=grafana" -o jsonpath="{.items[0].metadata.name}")
+POD_NAME=$(kubectl get pods --namespace monitoring -l "app.kubernetes.io/name=grafana" -o jsonpath="{.items[0].metadata.name}")
+# POD_NAME=$(kubectl get pods --namespace monitoring -l "app.kubernetes.io/instance=grafana" -o jsonpath="{.items[0].metadata.name}")
 
 kubectl --namespace monitoring port-forward $POD_NAME 3000 &
 
