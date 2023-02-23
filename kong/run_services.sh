f39ac437169207ddd08fb45d4599cc5a36b06f6d
Amit Karpe <amitkarpe@gmail.com>
2023-02-23 09:45:17 +0000

    With old version we got success. Using old KIC i.e. ingressController.installCRDs=false .

--- kong/run_services.sh
+++ kong/run_services.sh
@@ -5,18 +5,18 @@ echo "Run prometheus"
 POD_NAME=$(kubectl get pods --namespace monitoring -l "app.kubernetes.io/name=prometheus" -o jsonpath="{.items[0].metadata.name}")
 # POD_NAME=$(kubectl get pods --namespace monitoring -l "app=prometheus,component=server" -o jsonpath="{.items[0].metadata.name}")
 echo $POD_NAME
-kubectl --namespace monitoring port-forward $POD_NAME 9090 &
+kubectl --namespace monitoring port-forward $POD_NAME 9090  --address 0.0.0.0 &
 sleep 2
 
 echo "Run grafana"
 POD_NAME=$(kubectl get pods --namespace monitoring -l "app.kubernetes.io/name=grafana" -o jsonpath="{.items[0].metadata.name}")
 # POD_NAME=$(kubectl get pods --namespace monitoring -l "app.kubernetes.io/instance=grafana" -o jsonpath="{.items[0].metadata.name}")
 echo $POD_NAME
-kubectl --namespace monitoring port-forward $POD_NAME 3000 &
+kubectl --namespace monitoring port-forward $POD_NAME 3000  --address 0.0.0.0 &
 sleep 2
 
 echo "Run kong"
 POD_NAME=$(kubectl get pods --namespace kong -l "app.kubernetes.io/name=kong" -o jsonpath="{.items[0].metadata.name}")
 echo $POD_NAME
-kubectl --namespace kong port-forward $POD_NAME 8000 &
+kubectl --namespace kong port-forward $POD_NAME 8000  --address 0.0.0.0 &
 sleep 2
